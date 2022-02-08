671fd0acb305e27d8df4808a43fecaa45208abac
Freeda Francis <93197553+Freeda-F@users.noreply.github.com>
2022-02-08 18:48:45 +0530

    container-check.sh

---
+++ container-check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export CNAME=springboot-app
+if [ "$(docker ps -qa -f name=$CNAME)" ]; then
+echo ":: Found container - $CNAME"
+if [ "$(docker ps -q -f name=$CNAME)" ]; then
+echo ":: Stopping running container - $CNAME"
+docker stop $CNAME;
+fi
+echo ":: Removing stopped container - $CNAME"
+docker rm $CNAME;
+fi
