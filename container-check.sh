b8dcee93dc0779a08caa4c63d7c2055d5ddd3435
Freeda Francis <93197553+Freeda-F@users.noreply.github.com>
2022-02-09 11:06:04 +0530

    Update container-check.sh

--- container-check.sh
+++ container-check.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 export CNAME=springboot-app
-if [ "$(docker ps -qa -f name=$CNAME)" ]; then
-echo ":: Found container - $CNAME"
+
 if [ "$(docker ps -q -f name=$CNAME)" ]; then
-echo ":: Stopping running container - $CNAME"
-docker stop $CNAME;
+echo "Removing the container:$CNAME"
+docker rm -f $CNAME;
 fi
-echo ":: Removing stopped container - $CNAME"
-docker rm $CNAME;
-fi
+
+echo "Creating new container:$CNAME"
+docker container run --name springboot-app -p 8080:8080 -d freedafrancis/java-springboot-app:v2
